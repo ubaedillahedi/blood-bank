94a7beb7f1d9181a09d2f22d0a2f86ee10cbfe84
Edi Ubaedillah <ubaedillahedi@gmail.com>
2023-05-27 15:42:33 +0700

    add api current user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const mongoose = require("../config/dbConfig");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/register", async (req, res) => {
   try {
@@ -36,7 +37,6 @@ router.post("/register", async (req, res) => {
   }
 });
 
-// login user
 router.post("/login", async (req, res) => {
   try {
     // check if user exists
@@ -74,6 +74,22 @@ router.post("/login", async (req, res) => {
       success: true,
       message: "User logged in successfully",
       data: token,
+    });
+  } catch (error) {
+    return res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.get("/get-current-user", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.body.userId });
+    return res.send({
+      success: true,
+      message: "User fetched successfully",
+      data: user,
     });
   } catch (error) {
     return res.send({
