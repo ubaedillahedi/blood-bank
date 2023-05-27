818937da2bdcaac0c60d3b971d91ff5ee86dbb21
Edi Ubaedillah <ubaedillahedi@gmail.com>
2023-05-27 15:05:36 +0700

    Create connection to database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5001;
+const dbConfig = require("./config/dbConfig");
 
 app.listen(port, () => {
   console.log(`Node JS Server Started at ${port}`);
---
+++ config/dbConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.mongo_url);
+
+const connection = mongoose.connection;
+
+// verify connection
+connection.on("connected", () => {
+  console.log("Mongo DB Connection Successfull");
+});
+
+// verify connection failed
+connection.on("error", (err) => {
+  console.log("Mongo DB Connection Error ", err);
+});
